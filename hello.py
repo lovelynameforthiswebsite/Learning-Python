91273f81b4fe14aa49c4800049e3589a5ced148e
Alireza Cheriki <ali.ch.mt.57@gmail.com>
2023-12-31 17:02:28 +0330

    by typing a coma after the (''hello,'' ) ...  you could add a variable to the string and the ''return value'' will be added to the same function which we are adding other values in it... in this commit this is apearant

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 # ask user for their name
 name = input("what's your name? ")
-
+"""
+you can write nots with three double quotations
+"""
 # say hello to user
-print("hello,")
-print(name)
+print("hello," , name)
