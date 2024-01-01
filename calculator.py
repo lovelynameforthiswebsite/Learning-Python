d8cca901f5da6f13db44f4cc4dab5293df952703
Alireza Cheriki <ali.ch.mt.57@gmail.com>
2024-01-01 04:27:46 +0330

    --LEARNED HOW TO BUILD A CALCULATOT-- to do this we need to set a number of variables and then give them an '' input '' function for the user to set the value of the variable; we do this for the other variables too and then we will have another variable that's the combination of the defined variables ( in this case 'z') and then for python to recognize the numbers that user set as numbers ( python reads the input text as strings therefore as plain text which is not a number and therefore as a text it doesn't serve a purpose for the calculator) we define each adding variables as a '' int '' function's value and with a plus sign ( + ) between them, we reffer the compeleted string back to 'z'

---
+++ calculator.py
@@ -0,0 +1,4 @@
+x = input("what's x ? ")
+y = input("what's y ? ")
+z = int(x) + int(y)
+print(z)
--- hello.py
+++ hello.py
@@ -1,9 +1,14 @@
 # ask user for their name
-name = input("why do you stink sir? ").strip()
+name = input("what's your name? ").strip()
 name = name.title()
+
+# split user's name into first name and last name
+first , last = name.split(" ")
+
+
 # say hello to user
-print("well" , end="\n")
-print(f"\"  {name}  \"" , end=" ")
-print("is no excuse, you stink!")
+print("hello" , end="\n")
+print(f"\"  {first}  \"" , end=" ")
+print("nice to hear from you")
 
 
