2299eca0bf2e818d34e1c1002f45c32d4db188fe
Alireza Cheriki <ali.ch.mt.57@gmail.com>
2024-01-04 04:41:48 +0330

    in math integer is a group of numbers which only can be whole, and from +infinit to -infinit which is used here as an ''int'' function to define an number input as an text, As a integir number,,, same here goes for the ''float'' function which defines the number as a Real number which in math is a group of number that include a desimal in i, from +infinity to -infinity,,, this way our calculator is much more capable and precise

--- calculator.py
+++ calculator.py
@@ -4,6 +4,11 @@
     print(z)
 """
  # the seconde way to make an calculator
-x = int(input("what's x ? "))
-y = int(input("what's y ? "))
+""" x = int(input("what's x ? "))
+    y = int(input("what's y ? "))
+    print(x + y)
+"""
+# creating a calculator using floats
+x = float(input("what's x ? "))
+y = float(input("what's y ? "))
 print(x + y)
