76cb198f4364b73ad0275abb890b6706bb5f6983
Alireza Cheriki <ali.ch.mt.57@gmail.com>
2024-01-04 04:17:51 +0330

    the second way to make a calculator is to put the ''input'' function in side of an ''int'' function which make's the result of the input function to be the ''int''  functions argument and then instead of using an Z variable as the adding variable, we use the variables it selfs the argument of the print function and set and add sign between them

--- calculator.py
+++ calculator.py
@@ -1,4 +1,9 @@
-x = input("what's x ? ")
-y = input("what's y ? ")
-z = int(x) + int(y)
-print(z)
+""" x = input("what's x ? ")
+    y = input("what's y ? ")
+    z = int(x) + int(y)
+    print(z)
+"""
+ # the seconde way to make an calculator
+x = int(input("what's x ? "))
+y = int(input("what's y ? "))
+print(x + y)
