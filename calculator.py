4a3b2814ed4275cda3a7e73b3e32a85479a81863
lovelynameforthiswebsite <139525295+lovelynameforthiswebsite@users.noreply.github.com>
2024-01-04 22:55:29 +0330

    added some changes to the calculator just for practice

--- calculator.py
+++ calculator.py
@@ -20,9 +20,30 @@
     print(round(x / y ,3))
 """
 # makei the result of the equation to be seperated by a coma if it was a integer number of more than 4 numbers
-x = float(input("what's x ? "))
-y = float(input("what's y ? "))
+""" 
+    x = float(input("what's x ? "))
+    y = float(input("what's y ? "))
 
-z = (x * y)
+    z = (x * y)
 
-print(f"{z:.2f}")
+    print(f"{z:,}")
+"""
+# make the equation result be written to at # # most 3 digits if the the result contain's # # more than 3 digits
+
+def main()
+
+    x = float(input(what's x ? ))
+    y = float(input(what's y ? ))
+    print("x + y is",x + y)
+    print("the equation squared is", square(x + y))
+
+
+def square(n)
+    return n * n 
+
+
+
+
+main()
+    
+ 
