62e6b2676b21977ab0b170a5478695f5b5623c00
Alireza Cheriki <ali.ch.mt.57@gmail.com>
2024-01-08 03:28:35 +0330

    pulled the deleted calculator.py from past commits

---
+++ calculator.py
@@ -0,0 +1,48 @@
+""" x = input("what's x ? ")
+    y = input("what's y ? ")
+    z = int(x) + int(y)
+    print(z)
+"""
+ # the seconde way to make an calculator
+""" x = int(input("what's x ? "))
+    y = int(input("what's y ? "))
+    print(x + y)
+"""
+# creating a calculator using floats
+""" x = float(input("what's x ? "))
+    y = float(input("what's y ? "))
+    print(x + y)
+"""
+# Round the number
+"""round(number, ndigits=None)"""
+""" x = float(input("what's x ?"))
+    y = float(input("what's y ?"))
+    print(round(x / y ,3))
+"""
+# makei the result of the equation to be seperated by a coma if it was a integer number of more than 4 numbers
+"""
+    x = float(input("what's x ? "))
+    y = float(input("what's y ? "))
+
+    z = (x * y)
+
+    print(f"{z:,}")
+"""
+# make the equation result be written to at # # most 3 digits if the the result contain's # # more than 3 digits
+
+def main()
+
+    x = float(input(what's x ? ))
+    y = float(input(what's y ? ))
+    print("x + y is", x + y)
+    print("the equation squared is", square(x + y))
+
+
+def square(n)
+    return n * n
+
+
+
+
+main()
+
