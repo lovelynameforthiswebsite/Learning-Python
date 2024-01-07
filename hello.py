402256f7fe8c9ad18affd477ecd94534f96d69eb
Alireza Cheriki <ali.ch.mt.57@gmail.com>
2024-01-08 02:41:54 +0330

    == THE END OF VARIABLE AND FUNCTIONS LESSON ==

--- hello.py
+++ hello.py
@@ -1,14 +1,8 @@
 # ask user for their name
-name = input("what's your name? ").strip()
-name = name.title()
-
+name = input("what's your name ? ").title().strip()
 # split user's name into first name and last name
-first , last = name.split(" ")
+first ,last= name.split(" ")
 
 
 # say hello to user
-print("hello" , end="\n")
-print(f"\"  {first}  \"" , end=" ")
-print("nice to hear from you")
-
-
+print("hello",f"{first}")
