67009562e0c3f3d77036789ce562893b3db79da7
Alireza Cheriki <ali.ch.mt.57@gmail.com>
2024-01-08 07:50:09 +0330

    --LEARNED THE '' and '' statement-- with and you can ask one, two or more questions at the same time like the code below. there are better ways to write the code below. check the next two commits<<<<SECOND COMMIT>>>>: in python, instead of each time writing that 'score' is less than 100 and more than 90, we could simply write : 90 < score <= 100

---
+++ CONDITIONALS/grade.py
@@ -0,0 +1,41 @@
+def main():
+    """
+    score = float(input("what's the student's score ? "))
+    if score > 90 and score <= 100:
+        print(" A ")
+    elif score > 80 and score <= 90:
+        print(" B ")
+    elif score > 70 and score <= 80:
+        print(" C ")
+    elif score > 60 and score <= 50:
+        print(" D ")
+    else:
+        print(" F ")
+    """
+    score = float(input("what's the student's score ? "))
+    if 90 < score <= 100:
+        print(" A ")
+    elif 80 < score <= 90:
+        print(" B ")
+    elif 70 < score <= 80:
+        print(" C ")
+    elif 60 < score <= 50:
+        print(" D ")
+    else:
+        print(" F ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main()
