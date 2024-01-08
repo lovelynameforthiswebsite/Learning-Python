ca3c53a9b3baf221e4af7ee716a96080651d01e5
Alireza Cheriki <ali.ch.mt.57@gmail.com>
2024-01-08 05:01:04 +0330

    ++NEW CHAPTER: CONDITIONALS++ --LEARNED THE '' if '' SYNTAX AND THE MATHMATICAL SYMBOLS IN PYTHON-- conditionals are used to awnser the programs questions based on the awnser it gets. the '' if '' syntax allowes us to do such thing. as it seems in the code below, there are conditions set by the programer which if awnsered, based on the awnser it will print different awnsers to the question. in this case the '' if '' statement is explained by the simple mathmatical symbols between two variables...the '' < '' , '' > '' , '' <= '' and '' >= '' is simple but '' == '' is used instead of only one '' = '' because only one '' = '' means the right side of the '' = '' refferes to the left side, IT DOSE NOT MEANS THE RIGHT AND THE LEFT SIDE ARE EQUAL...but with two equal symbols '' == '' it actually means that the right side and the left side are equal and we can use them for numbers here

---
+++ CONDITIONALS/compare.py
@@ -0,0 +1,26 @@
+"""
+    >
+    >=
+    <
+    <=
+    ==
+    !=
+
+"""
+def main():
+    x = int(input("what's x ? "))
+    y = int(input("what's y ? "))
+
+    if x < y:
+        print("x is less than y ")
+    if x > y:
+        print("x is more than y ")
+    if x == y:
+        print("x is equal to y ")
+
+
+
+
+
+
+main()
