f9c6e1586353ca0cf82fe3db8d47cf49e8880f85
Alireza Cheriki <ali.ch.mt.57@gmail.com>
2024-01-08 06:58:42 +0330

    --LEARNED THE '' or '' statement-- the or statement lets us ask two questions in one line of syntax and they both well be reffering to one awnser,.... in this case below we could use ( x != y ) which means x is not equal to y and it even gives us a faster awnser rather than x < y or x > y

--- CONDITIONALS/compare.py
+++ CONDITIONALS/compare.py
@@ -1,20 +1,10 @@
-"""
-    >
-    >=
-    <
-    <=
-    ==
-    !=
 
-"""
 def main():
     x = int(input("what's x ? "))
     y = int(input("what's y ? "))
 
-    if x < y:
-        print("x is less than y ")
-    elif x > y:
-        print("x is more than y ")
+    if x < y or x > y
+        print("x is not equal to y ")
     else:
         print("x is equal to y ")
 
